12e9a93cd1f536144b94ea1084f22366df263ff9
AsteroidQiao <2551845461@qq.com>
2022-11-26 15:43:20 +0800

    Initial commit

---
+++ src/test/java/com/qiao/association/AssociationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiao.association;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssociationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
